38b80e4a5d9a7d24d91143aea9903559bc5767c2
edithcabezas <ecabezas1984@gmail.com>
2024-11-09 08:40:11 -0500

    primer commit

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+numero1 = int(input("Ingresar el primer numero: "))
+numero2 = int(input("Ingresar el segundo numero: "))
+resta = numero1-numero2
+print("La resta es:\t"+str(resta)) 
+ 
+
+quit(0)
+numero1 = int(input("Ingresar el primer numero: "))
+numero2 = int(input("Ingresar el segundo numero: "))
+suma = numero1+numero2
+print("La suma es:\t"+str(suma)) 
+
+numero1 = int(input("Ingresar el primer numero: "))
+numero2 = int(input("Ingresar el segundo numero: "))
+multiplicacion = numero1*numero2
+print("La multiplicacion es:\t"+str(multiplicacion)) 
+
+numero1 = int(input("Ingresar el primer numero: "))
+numero2 = int(input("Ingresar el segundo numero: "))
+division = numero1/numero2
+print("La division es:\t"+str(division)) 
